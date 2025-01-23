0e46729a18c5cad01cbb23f92f7c04044fc86cd4
oceanbig093 <innervision41@mail.ru>
2025-01-23 16:39:11 +0300

    Update module13_6.py

--- module13_6.py
+++ module13_6.py
@@ -6,7 +6,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import asyncio
 
-api = '7943929055:AAEtMwj6jCTUKFt-KPZvtXanGQYRn5gYlSo'
+api = ''
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
